4c3f29392102befc7d11520f797e0c2e6e6dd320
[AI8/1반] 박현진 <dev.hyunjin.park@gmail.com>
2023-08-14 23:15:45 +0900

    환경세팅, 라우팅

---
+++ front/src/App.js
@@ -0,0 +1,23 @@
+import * as React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Main from "./pages/Main";
+import Login from "./pages/Login";
+import Join from "./pages/Join";
+import MyProfile from "./pages/MyProfile";
+import Search from "./pages/Search";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" exact element={<Main />} />
+        <Route path="/login" exact element={<Login />} />
+        <Route path="/register" exact element={<Join />} />
+        <Route path="/users/:id" exact element={<MyProfile />} />
+        <Route path="/search" exact element={<Search />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
---
+++ front/src/pages/Join.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Join = () => {
+  return <div>Join Page</div>;
+};
+
+export default Join;
