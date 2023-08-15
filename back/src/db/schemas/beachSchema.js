710f9203d6282f8b7f73f2c59f74e5f322177e8b
mixsam <mixsam.taurusium@gmail.com>
2023-08-15 21:38:42 +0900

    fix:beachSchema, fixed

--- back/src/db/schemas/beachSchema.js
+++ back/src/db/schemas/beachSchema.js
@@ -1,1 +1,46 @@
-// git upload
+import { Schema, model } from "mongoose";
+
+const BeachSchema = new Schema(
+  {
+    id: {
+      type: Number,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    latitude: {
+      type: Number,
+      required: false,
+    },
+    longtitude: {
+      type: Number,
+      required: false,
+    },
+    goodnessFit: {
+      type: Boolean,
+      required: false,
+    },
+    score: {
+      type: Number,
+      required: false,
+    },
+    ente: {
+      type: Number,
+      required: false,
+    },
+    esch: {
+      type: Number,
+      required: false,
+    },
+  }
+);
+
+const BeachModel = model("Beach", BeachSchema);
+
+export { BeachModel };
