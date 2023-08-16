107c71a02e5b5f57f5161ec54e4d5f90434dd930
[AI8/1반] 이도은 <delee1104@gmail.com>
2023-08-16 16:01:41 +0900

    feature/front_button,footer

--- front/src/App.js
+++ front/src/App.js
@@ -5,6 +5,7 @@ import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
 import MyProfile from "./pages/MyProfile";
 import Search from "./pages/Search";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         <Route path="/users/:id" exact element={<MyProfile />} />
         <Route path="/search" exact element={<Search />} />
       </Routes>
+      <Footer />
     </Router>
   );
 }
