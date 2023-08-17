799698bd57191e6c07dbf9558a0644a14ea419c7
[AI8/1반] 박현진 <dev.hyunjin.park@gmail.com>
2023-08-17 23:44:46 +0900

    ss

---
+++ front/src/components/AddReview.js
@@ -0,0 +1,86 @@
+import React, { useState } from "react";
+import {
+  Button,
+  Col,
+  FloatingLabel,
+  Image,
+  InputGroup,
+  Modal,
+  Row,
+} from "react-bootstrap";
+import Avatar from "./Avatar";
+import { Form } from "react-router-dom";
+
+const AddReview = ({ setShowModal, showModal }) => {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const handleClose = () => setShowModal(false);
+  return (
+    <Modal
+      centered
+      show={showModal}
+      onHide={handleClose}
+      onClick={(e) => e.stopPropagation()}
+      // 이벤트 전파 방지용 >> 없을 시 모달창 클릭할 때도 모달창이 사라지는 현상
+    >
+      <Modal.Header closeButton>
+        <Modal.Title>새 게시물 작성하기</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <Row>
+          <Col xs={8}>
+            <Image
+              fluid
+              style={{ width: "100%" }}
+              src="https://health.chosun.com/site/data/img_dir/2023/05/31/2023053102582_0.jpg"
+            ></Image>
+          </Col>
+          <Col xs={4}>
+            <Row>
+              <Col xs="auto">
+                <Avatar width="30" />
+              </Col>
+              <Col className="px-0 d-flex align-items-center">훈제오리</Col>
+            </Row>
+            <InputGroup className="">
+              <FloatingLabel
+                controlId="floatingInput"
+                label="제목"
+                className="my-2"
+              >
+                <Form.Control
+                  value={title}
+                  onChange={(e) => setTitle(e.target.value)}
+                />
+              </FloatingLabel>
+              <div>
+                {/* column 정렬을 위한 div입니다. FloatingLabel 때문에 css로 정렬하면 레이아웃 망가짐. */}
+                <Form.Control
+                  value={content}
+                  onChange={(e) => {
+                    if (e.target.value.length <= 300) {
+                      setContent(e.target.value);
+                    }
+                  }}
+                  placeholder="내용"
+                  as="textarea"
+                  rows={3}
+                  style={{ height: "100%" }}
+                />
+              </div>
+            </InputGroup>
+            <small className="text-muted">
+              {content ? content.length : "0"}/300
+            </small>
+          </Col>
+        </Row>
+      </Modal.Body>
+
+      <Modal.Footer>
+        <Button variant="primary">공유</Button>
+      </Modal.Footer>
+    </Modal>
+  );
+};
+
+export default AddReview;
--- front/src/components/NavBar.js
+++ front/src/components/NavBar.js
@@ -4,31 +4,19 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import { useNavigate } from "react-router-dom";
-import {
-  Button,
-  Col,
-  FloatingLabel,
-  Form,
-  Image,
-  InputGroup,
-  Modal,
-  OverlayTrigger,
-  Row,
-  Tooltip,
-} from "react-bootstrap";
+import { OverlayTrigger, Tooltip } from "react-bootstrap";
 import { faCompass } from "@fortawesome/free-solid-svg-icons";
 import Avatar from "./Avatar";
 import { faSquarePlus } from "@fortawesome/free-regular-svg-icons";
 import { useState } from "react";
+import AddReview from "./AddReview";
 
 const NavBar = () => {
   const [showModal, setShowModal] = useState(false);
-  const [title, setTitle] = useState("");
-  const [content, setContent] = useState("");
+
   const navigate = useNavigate();
 
   const handleNavigate = (path) => navigate(path);
-  const handleClose = () => setShowModal(false);
 
   return (
     <>
@@ -67,72 +55,7 @@ const NavBar = () => {
                 <FontAwesomeIcon icon={faSquarePlus} />
               </OverlayTrigger>
               {/* 클릭하면 오버레이? */}
-              <Modal
-                centered
-                show={showModal}
-                onHide={handleClose}
-                onClick={(e) => e.stopPropagation()}
-                // 이벤트 전파 방지용 >> 없을 시 모달창 클릭할 때도 모달창이 사라지는 현상
-              >
-                <Modal.Header closeButton>
-                  <Modal.Title>새 게시물 작성하기</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                  <Row>
-                    <Col xs={8}>
-                      <Image
-                        fluid
-                        style={{ width: "100%" }}
-                        src="https://health.chosun.com/site/data/img_dir/2023/05/31/2023053102582_0.jpg"
-                      ></Image>
-                    </Col>
-                    <Col xs={4}>
-                      <Row>
-                        <Col xs="auto">
-                          <Avatar width="30" />
-                        </Col>
-                        <Col className="px-0 d-flex align-items-center">
-                          훈제오리
-                        </Col>
-                      </Row>
-                      <InputGroup className="">
-                        <FloatingLabel
-                          controlId="floatingInput"
-                          label="제목"
-                          className="my-2"
-                        >
-                          <Form.Control
-                            value={title}
-                            onChange={(e) => setTitle(e.target.value)}
-                          />
-                        </FloatingLabel>
-                        <div>
-                          {/* column 정렬을 위한 div입니다. FloatingLabel 때문에 css로 정렬하면 레이아웃 망가짐. */}
-                          <Form.Control
-                            value={content}
-                            onChange={(e) => {
-                              if (e.target.value.length <= 300) {
-                                setContent(e.target.value);
-                              }
-                            }}
-                            placeholder="내용"
-                            as="textarea"
-                            rows={3}
-                            style={{ height: "100%" }}
-                          />
-                        </div>
-                      </InputGroup>
-                      <small className="text-muted">
-                        {content ? content.length : "0"}/300
-                      </small>
-                    </Col>
-                  </Row>
-                </Modal.Body>
-
-                <Modal.Footer>
-                  <Button variant="primary">공유</Button>
-                </Modal.Footer>
-              </Modal>
+              <AddReview setShowModal={setShowModal} showModal={showModal} />
             </Nav.Link>
 
             {/* TO DO: 로그인 했다면 '로그아웃'으로 변경 */}
