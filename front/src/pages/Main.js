1ae0a670cf5d8744666eaf38dac346d29fbb6370
[AI8/1반] 이도은 <delee1104@gmail.com>
2023-08-19 00:07:08 +0900

    feature/front2

--- front/src/pages/Main.js
+++ front/src/pages/Main.js
@@ -1,16 +1,16 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import SearchBar from "../components/SearchBar";
+// import SearchBar from "../components/SearchBar";
 
 const Main = () => {
   return (
     <Container fluid style={{ height: "100vh" }}>
       <Row style={{ height: "100%" }}>
         <Col className="px-0">
-          <SearchBar />
+          {/* <SearchBar /> */}
         </Col>
         <Col className="px-0">
-          <div>graph container</div>
+          <div>main container</div>
         </Col>
       </Row>
     </Container>
