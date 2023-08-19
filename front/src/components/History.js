3f0226ba5586c6fad94e190199aedc18b330612d
[AI8/1반] 임재홍 <nerineririn@gmail.com>
2023-08-19 12:07:30 +0900

    Fix: 머지 작업을 위한 커밋 - 2

--- front/src/components/History.js
+++ front/src/components/History.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react'
 import { Col, Container, Row } from "react-bootstrap";
 
 const History = () => {
@@ -16,7 +16,7 @@ const History = () => {
         </Row>
       </Container>
     </>
-  );
-};
+  )
+}
 
 export default History;
