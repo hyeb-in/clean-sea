05d090c0f3e1fd15d3135492f191b9cf682fda51
[AI8/1반] 임재홍 <nerineririn@gmail.com>
2023-08-19 14:25:06 +0900

    Feat: users api 이용해서 유저 정보 가져오도록 변경

--- front/src/pages/MyProfile.js
+++ front/src/pages/MyProfile.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useContext } from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   Container,
   Row,
@@ -9,28 +9,37 @@ import {
   Button,
   FormControl
 } from "react-bootstrap";
+import { useParams } from 'react-router-dom';
 import History from "../components/History";
 import CardHeader from "react-bootstrap/CardHeader";
-import { UserStateContext } from "../App";
 import * as Api from "../Api";
 
 const MyProfile = () => {
-  const userState = useContext(UserStateContext);
+  const { id } = useParams();
 
-  const [userName, setUserName] = useState(userState.user?.userName || "훈제오리");
-  const [userEmail, setUserEmail] = useState(userState.user?.userEmail || "elice@elice.com");
-  const [userDescription, setUserDescription] = useState(userState.user?.userDescription || "설명이 아직 없습니다. 추가해주세요");
-  const [userProfileImage, setuserProfileImage] = useState(userState.user?.profileImage || "https://blog.getbootstrap.com/assets/brand/bootstrap-logo-shadow@2x.png");
+  const [userName, setUserName] = useState("훈제오리");
+  const [userEmail, setUserEmail] = useState("elice@elice.com");
+  const [userDescription, setUserDescription] = useState("설명이 아직 없습니다. 추가해주세요");
+  const [userProfileImage, setUserProfileImage] = useState("https://blog.getbootstrap.com/assets/brand/bootstrap-logo-shadow@2x.png");
+  const [isEditMode, setIsEditMode] = useState(false);
 
   useEffect(() => {
-    if(userState.user) {
-      setUserName(userState.user.userName);
-      setUserEmail(userState.user.userEmail);
-      setUserDescription(userState.user.userDescription);
-    }
-  }, [userState]);
+    const fetchUserData = async () => {
+      try {
+        const response = await Api.get(`users/${id}`);
+        const userData = response.data;
 
-  const [isEditMode, setIsEditMode] = useState(false);
+        setUserName(userData.name);
+        setUserEmail(userData.email);
+        setUserDescription(userData.description);
+        setUserProfileImage(userData.profileImage || "https://blog.getbootstrap.com/assets/brand/bootstrap-logo-shadow@2x.png");
+      } catch (error) {
+        alert('유저 정보를 가져오는 데 실패했습니다.');
+      }
+    };
+
+    fetchUserData();
+  }, [id]);
 
   const handleEditClick = () => {
     setIsEditMode(!isEditMode);
@@ -39,13 +48,12 @@ const MyProfile = () => {
   const handleCompleteClick = async () => {
     setIsEditMode(false);
 
-    const apiEndpoint = `/users/${userState.user.userId}`;
+    const apiEndpoint = `/users/${id}`;
     const postData = {
       name: userName,
       email: userEmail,
       description: userDescription
     };
-
     try {
       await Api.put(apiEndpoint, postData);
       alert('정보가 성공적으로 업데이트되었습니다.');
@@ -68,34 +76,19 @@ const MyProfile = () => {
                   alt="User Profile"
                 />
               </div>
-              {
-                isEditMode
-                  ? <FormControl type="text" value={userName} onChange={e => setUserName(e.target.value)} />
-                  : <h4 className="mb-2">{userName}</h4>
-              }
-              {
-                isEditMode
-                  ? <FormControl type="text" value={userEmail} onChange={e => setUserEmail(e.target.value)} />
-                  : <span className="text-muted d-block mb-1">{userEmail}</span>
-              }
+              {isEditMode ? <FormControl type="text" value={userName} onChange={e => setUserName(e.target.value)} /> : <h4 className="mb-2">{userName}</h4>}
+              {isEditMode ? <FormControl type="text" value={userEmail} onChange={e => setUserEmail(e.target.value)} /> : <span className="text-muted d-block mb-1">{userEmail}</span>}
             </CardHeader>
             <ListGroup flush>
               <ListGroupItem className="p-4">
-                {
-                  isEditMode
-                    ? <FormControl type="text" value={userDescription} onChange={e => setUserDescription(e.target.value)} />
-                    : <strong className="text-muted d-block mb-2">{userDescription}</strong>
-                }
+                {isEditMode ? <FormControl type="text" value={userDescription} onChange={e => setUserDescription(e.target.value)} /> : <strong className="text-muted d-block mb-2">{userDescription}</strong>}
               </ListGroupItem>
             </ListGroup>
 
             <span>
-              <Button variant="link" onClick={handleEditClick}>
-                {isEditMode ? '취소' : '편집'}
-              </Button>
+              <Button variant="link" onClick={handleEditClick}>{isEditMode ? '취소' : '편집'}</Button>
               {isEditMode && <Button variant="link" onClick={handleCompleteClick}>완료</Button>}
             </span>
-
           </Card>
         </Col>
       </Row>
