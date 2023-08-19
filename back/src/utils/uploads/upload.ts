7a2b86bd6a0cb5c010301a127a81ae96e5ed4263
LeeChangGeun <echk1212@gmail.com>
2023-08-19 16:02:10 +0900

    Feat : reviewRouter

--- back/src/utils/uploads/upload.ts
+++ back/src/utils/uploads/upload.ts
@@ -1,35 +1,35 @@
-// import { FileObject, FileRequest } from "upload";
+import { FileObject, FileRequest } from "../../types/upload";
 
-// class FileAppender{
-//     strategy : string;
-//     req : FileRequest;
-//     constructor(strategy : string, req : FileRequest){
-//         this.strategy = strategy;
-//         this.req = req;
+class FileAppender{
+    strategy : string;
+    req : FileRequest;
+    constructor(strategy : string, req : FileRequest){
+        this.strategy = strategy;
+        this.req = req;
 
-//         switch (strategy){
-//             case "NONE":
-//                 break;
-//             case "VALUE":
-//                 break;
-//             case "ARRAY":
-//                 req.files = [];
-//                 break;
+        switch (strategy){
+            case "NONE":
+                break;
+            case "VALUE":
+                break;
+            // case "ARRAY":
+            //     this.req.files = [];
+            //     break;
 
-//             default :
-//             throw new Error('Unknown file strategy :' + strategy);
-//         }
-//     }
+            default :
+                throw new Error('Unknown file strategy :' + strategy);
+        }
+    }
 
-//     replacePlaceholder(placeholder : FileObject, file : FileObject){
-//         if (this.strategy === "VALUE"){
-//             this.req.file = file;
-//             return;
-//         }
+    replacePlaceholder(placeholder : FileObject, file : FileObject){
+        if (this.strategy === "VALUE"){
+            this.req.file = file;
+            return;
+        }
 
-//         delete placeholder.fieldname;
-//         Object.assign(placeholder, file);
-//     }
-// }
+        delete placeholder.fieldname;
+        Object.assign(placeholder, file);
+    }
+}
 
-// export { FileAppender };
+export { FileAppender };
