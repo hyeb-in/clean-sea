687bac82a0f76ebda7c39eff1eb81df191a9ec23
[AI8/1반] 임재홍 <nerineririn@gmail.com>
2023-08-19 19:08:28 +0900

    Feat: 새로운 방문 로그 추가하는 로직, 업데이트와 같이 travel 관련 api가 동작하지 않는 문제 수정 중

--- front/src/components/History.js
+++ front/src/components/History.js
@@ -1,29 +1,44 @@
-import React, { useState, useEffect } from 'react'
-import {
-  Col,
-  Container,
-  Row,
-  Button,
-  FormControl,
-  Card
-} from "react-bootstrap";
+import React, { useState, useEffect, useContext  } from 'react'
+import { Col, Container, Row, Card, Button, Modal, FormControl } from "react-bootstrap";
 import * as Api from "../Api";
+import { UserStateContext } from "../App";
 
 const History = () => {
   const [isEditMode, setIsEditMode] = useState(false);
   const [travels, setTravels] = useState([]);
   const [editedTravel, setEditedTravel] = useState(null);
+  const [showModal, setShowModal] = useState(false);
+  const { user } = useContext(UserStateContext);
+  const [newTravel, setNewTravel] = useState({
+    author: user._id,
+    beachId: '',
+    date: ''
+  });
+
+  const handleModalClose = () => setShowModal(false);
+  const handleModalOpen = () => setShowModal(true);
+
+  const fetchTravelList = async () => {
+    try {
+      const response = await Api.get('travels/travelList');
+      setTravels(response);
+    } catch (error) {
+      console.error('방문 로그 조회 실패:', error);
+    }
+  };
+  const handleNewTravelSubmit = async () => {
+    try {
+      const travelData = { ...newTravel, date: new Date(newTravel.date) };
+      await Api.post('travels/register', { data: travelData });
+    } catch (error) {
+      console.error('새로운 여행 정보 등록 실패:', error);
+    }
+
+    await fetchTravelList();
+    handleModalClose();
+  };
 
   useEffect(() => {
-    const fetchTravelList = async () => {
-      try {
-        const response = await Api.get({ endpoint: 'travels/travelList' });
-        setTravels(response);
-      } catch (error) {
-        console.error('방문 로그 조회 실패:', error);
-      }
-    };
-
     fetchTravelList();
   }, []);
 
@@ -31,7 +46,7 @@ const History = () => {
     if (!editedTravel) return;
 
     try {
-      await Api.put({ endpoint: `travels/update/${editedTravel._id}`, data: editedTravel });
+      await Api.put(`travels/update/${editedTravel._id}`, editedTravel);
       const updatedTravels = travels.map(travel => travel._id === editedTravel._id ? editedTravel : travel);
       setTravels(updatedTravels);
       setIsEditMode(false);
@@ -49,6 +64,20 @@ const History = () => {
             <Row className="mt-4">
               <Col>
                 <h3 className="mb-3">방문 로그</h3>
+              </Col>
+              <Col xs={6} className="d-flex justify-content-end">
+                <Button
+                  onClick={handleModalOpen}
+                >
+                  로그 작성
+                </Button>
+              </Col>
+              <Col xs={1}>
+                {/*여백용 빈 컬럼*/}
+              </Col>
+            </Row>
+            <Row>
+              <Col>
                 {travels.length > 0 ? (
                   travels.map((travel, index) => (
                     <Row key={index}>
@@ -67,7 +96,7 @@ const History = () => {
                     </Row>
                   ))
                 ) : (
-                  <p className="mb-4">방문 로그가 없습니다</p>
+                  <p className="m-2">방문 로그가 없습니다</p>
                 )}
               </Col>
             </Row>
@@ -81,6 +110,34 @@ const History = () => {
             }
           </Card.Body>
         </Card>
+
+        <Modal show={showModal} onHide={handleModalClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>새 방문 로그 작성</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <FormControl
+              type="text"
+              placeholder="해수욕장 id"
+              value={newTravel.beachId}
+              onChange={(e) => setNewTravel({ ...newTravel, beachId: e.target.value })}
+            />
+            <FormControl
+              type="date"
+              className="mt-2"
+              value={newTravel.date}
+              onChange={(e) => setNewTravel({ ...newTravel, date: e.target.value })}
+            />
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleModalClose}>
+              취소
+            </Button>
+            <Button variant="primary" onClick={handleNewTravelSubmit}>
+              저장
+            </Button>
+          </Modal.Footer>
+        </Modal>
       </Container>
     </>
   )
