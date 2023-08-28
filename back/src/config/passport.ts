dec022cdf7c32a3bee600df282829a98e084355d
leehyebin <970905hyebin@gmail.com>
2023-08-28 18:50:07 +0900

    Feat:add user Validator message

--- back/src/config/passport.ts
+++ back/src/config/passport.ts
@@ -9,15 +9,28 @@ const localOptions = {
   passwordField: "password",
 };
 
+const jwtOptions = {
+  secretOrKey: process.env.JWT_SECRET_KEY,
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+};
+
 const localCallback = async (email: string, password: string, done: any) => {
+  // findOne함수 더 이상 callback 지원X
+
+  // await UserModel.findOne({ email }, (err: Error, user: IUser) => {
+  //   if (err) return done(err);
+  //   if (!user)
+  //     return done(null, false, { message: "회원이 존재하지 않습니다." });
+  //   if (!pwdMatchCheck(password, user))
+  //     return done(null, false, { message: "비밀번호가 일치하지 않습니다." });
+  //   done(null, user);
+  // });
   try {
     const user = await findUserByEmail(email);
     if (!user) {
-      return done(null, false, { message: "회원이 존재하지 않습니다.!" });
+      return done(null, false, { message: "사용자가 존재하지 않습니다." });
     }
-
     const isMatched = await bcrypt.compare(password, user.password);
-
     if (!isMatched) {
       return done(null, false, { message: "비밀번호가 일치하지 않습니다." });
     }
@@ -26,12 +39,6 @@ const localCallback = async (email: string, password: string, done: any) => {
     done(error);
   }
 };
-
-const jwtOptions = {
-  secretOrKey: process.env.JWT_SECRET_KEY,
-  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-};
-
 const jwtCallback = async (payload: any, done: any) => {
   try {
     const { id } = payload;
--- back/src/utils/validators/userValidator.ts
+++ back/src/utils/validators/userValidator.ts
@@ -6,6 +6,54 @@ import { errorGenerator } from "../errorGenerator";
 const nameReg: RegExp = /^[a-zA-Z가-힣]+$/;
 const passwordReg: RegExp = /^[a-zA-Z0-9!@#;:'-_=+,./?]+$/;
 
+const emailErrorMessage = {
+  "string.min": "Email은 5자 이상 입력해주십시오.",
+  "string.max": "Email은 30자 이하로 입력해주십시오.",
+  "any.required": "Email을 입력해주십시오.",
+  "string.email": "Eamil형식이 유효하지 않습니다.",
+};
+
+const nameErrormessage = {
+  "string.min": "이름은 2자 이상 입력해주십시오.",
+  "string.max": "이름은 20자 이하로 입력해주십시오.",
+  "any.required": "이름을 입력해주십시오.",
+  "string.pattern.base": "이름은 한글 또는 영문 대소문자만 입력가능합니다.",
+};
+
+const passwordErrorMessage = {
+  "string.min": "비밀번호는 4자 이상 입력해주십시오.",
+  "string.max": "비밀번호는 30자 이하로 입력해주십시오.",
+  "any.required": "비밀번호를 입력해주십시오.",
+  "string.pattern.base":
+    "비밀번호는 영문 대소문자, 숫자, 특수문자(!@#;:'-_=+,./?)만 입력가능합니다.",
+};
+
+const validateSchema = {
+  name: joi
+    .string()
+    .min(5)
+    .max(20)
+    .required()
+    .pattern(nameReg)
+    .messages({ ...nameErrormessage }),
+  email: joi
+    .string()
+    .min(5)
+    .max(30)
+    .required()
+    .email()
+    .messages({ ...emailErrorMessage }),
+  password: joi
+    .string()
+    .min(4)
+    .max(30)
+    .required()
+    .pattern(passwordReg)
+    .messages({ ...passwordErrorMessage }),
+  description: joi.string().optional(),
+  uploadFile: joi.any(),
+};
+
 export const validateSignUp = async (
   req: IRequest,
   res: Response,
@@ -13,15 +61,12 @@ export const validateSignUp = async (
 ) => {
   const { name, email, password } = req.body;
   const schema = joi.object({
-    name: joi.string().min(5).max(20).required().pattern(nameReg),
-    email: joi.string().min(5).max(30).required().email().messages({
-      "string.min": "Email은 최소 5자 이하로 입력해주세요",
-      "string.max": "Email은 30자 이하로 써주세요  ",
-    }),
-    password: joi.string().min(4).max(30).required().pattern(passwordReg),
+    name: validateSchema.name,
+    email: validateSchema.email,
+    password: validateSchema.password,
   });
 
-  const { value, error } = schema.validate({ name, email, password });
+  const { error } = schema.validate({ name, email, password });
 
   if (error) {
     const err = errorGenerator(error.details[0].message, 400);
@@ -39,11 +84,11 @@ export const validateLogin = (
   const { email, password } = req.body;
 
   const schema = joi.object({
-    email: joi.string().required().email(),
-    password: joi.string().min(4).max(30).required().pattern(passwordReg),
+    email: validateSchema.email,
+    password: validateSchema.password,
   });
 
-  const { value, error } = schema.validate({ email, password });
+  const { error } = schema.validate({ email, password });
 
   if (error) {
     const err = errorGenerator(error.details[0].message, 400);
@@ -59,16 +104,15 @@ export const validateUpdateUser = (
   next: NextFunction
 ) => {
   const updateValue = req.body;
-  console.log(req.body);
+
   const schema = joi.object({
-    name: joi.string().min(2).max(20).required().pattern(nameReg),
-    description: joi.string().optional(),
-    // profileImage: joi.string().optional(),
-    uploadFile: joi.any(),
+    name: validateSchema.name,
+    description: validateSchema.description,
+    uploadFile: validateSchema.uploadFile,
   });
 
-  const { value, error } = schema.validate(updateValue);
-  console.log(value);
+  const { error } = schema.validate(updateValue);
+
   if (error) {
     const err = errorGenerator(error.details[0].message, 400);
     next(err);
